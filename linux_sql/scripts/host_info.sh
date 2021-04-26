b3bbba38bad385a4b323a356184cf229530a4b5e
Tina Lee <tinalee262008@gmail.com>
2021-04-26 19:54:37 +0000

    Add usage script and modify insert statement

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,3 +1,5 @@
+# Check if arguments are valid
+
 # Assign command line arguments
 psql_host=$1
 psql_port=$2
@@ -5,13 +7,14 @@ db_name=$3
 psql_user=$4
 psql_password=$5
 
-specs="$lscpu"
+specs=`lscpu`
 
 # helper function to fetch hardware information
 fetch_info() {
   echo "$specs" | grep -E "$1" | awk -F ':' '{print $2}' | xargs
 }
 
+# Get hardware information
 hostname=$(hostname -f)
 cpu_number=$(fetch_info "^CPU\(s\):")
 cpu_architecture=$(fetch_info "^Arch")
@@ -23,8 +26,9 @@ timestamp=$(date "+%Y-%m-%d %T")
 
 # Insert statement into psql
 statement="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp)
-          VALUES ($hostname, $cpu_number, $cpu_architecture, $cpu_model, $cpu_mhz, $l2_cache, $total_mem, $timestamp);"
+           VALUES ('${hostname}', '${cpu_number}', '${cpu_architecture}', '${cpu_model}', '${cpu_mhz}', '${l2_cache}', '${total_mem}', '${timestamp}');"
 
-psql -h "$psql_host" -U "$psql_user" -d "$db_name" -p "$psql_port" -c "$statement"
+PGPASSWORD="$(psql_password)"
+psql -h $psql_host -p $psql_port -U $psql_user -d $db_name -c "$statement"
 
 exit 0;
