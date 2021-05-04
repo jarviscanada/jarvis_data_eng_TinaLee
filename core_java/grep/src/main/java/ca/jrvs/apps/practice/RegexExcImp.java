7c82d231c11c284f938ed482cd09ca60318eadbc
Tina Lee <tinalee262008@gmail.com>
2021-05-04 10:30:56 -0700

    init RegexExc

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -0,0 +1,30 @@
+package ca.jrvs.apps.practice;
+
+import java.util.regex.Pattern;
+
+public class RegexExcImp implements RegexExc{
+
+  @Override
+  public boolean matchJpeg(String filename) {
+    return Pattern.matches(".*\\.jpe?g", filename);
+  }
+
+  @Override
+  public boolean matchIp(String ip) {
+    return Pattern.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}", ip);
+  }
+
+  @Override
+  public boolean isEmptyLine(String line) {
+    return Pattern.matches("^\\s*$", line);
+  }
+
+  public static void main(String[] args) {
+    RegexExcImp test = new RegexExcImp();
+    String ip = "1.2.3.4";
+    test.matchIp(ip);
+    test.matchJpeg("tina.jpeg");
+
+
+  }
+}
